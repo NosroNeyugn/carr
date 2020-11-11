0a1fbb98dd0f3f3de46abc02174981175d88926b
NosroNeyugn <69816627+NosroNeyugn@users.noreply.github.com>
2020-11-10 18:52:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var database, playerCount, form, game, player;
+var position, gameState = 0;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");  
+}
